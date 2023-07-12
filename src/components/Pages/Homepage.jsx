22e42c2a45fc071072b517aeb1204de24e4ea075
Gessmass <martinweilldrums@gmail.com>
2023-07-12 17:27:24 +0200

    sdf

--- src/components/Pages/Homepage.jsx
+++ src/components/Pages/Homepage.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import "./Homepage.scss";
+import backgroundHomepage from "../../assets/images/Homepage/backgroundHomepage.png";
+import Logo from "../../assets/images/Homepage/Logo.png";
 
 const Homepage = () => {
+  const [hidden, setHidden] = useState(false);
+
+  const handleHide = () => {
+    setHidden(true);
+  };
+
   return (
     <div>
-      <p>Homepage</p>
+      <div className={hidden === true ? "swipeUp" : "swipeDown"}>
+        <img className="backgroundHomepage" src={backgroundHomepage} />
+        <img className="logoOsook" src={Logo} />
+        <div className="divButtonSwipe">
+          <button type="button" onClick={handleHide} />
+        </div>
+      </div>
+      <div className="iciNavbar"></div>
     </div>
   );
 };
