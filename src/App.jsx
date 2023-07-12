c4a2c526a984b2cee16f69edfa8ffd09a2e230cd
HugoHtvl <hug.oo@hotmail.fr>
2023-07-12 17:19:27 +0200

    setting context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect, useMemo } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import MyContext from "./components/Context";
 import Homepage from "./components/Pages/Homepage.jsx";
 import Publier from "./components/Pages/Publier";
 import Favoris from "./components/Pages/Favoris";
@@ -7,16 +9,34 @@ import Messages from "./components/Pages/Messages";
 import Profil from "./components/Pages/Profil";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hugohtvl.github.io/o-sook-api/server/objects.json")
+      .then((res) => res.json())
+      .then((res) => setItems(res));
+  }, []);
+
+  const valuesInContext = useMemo(
+    () => ({
+      items,
+      setItems,
+    }),
+    [items, setItems]
+  );
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/publier" element={<Publier />} />
-        <Route path="/favoris" element={<Favoris />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/profil" element={<Profil />} />
-      </Routes>
-    </Router>
+    <MyContext.Provider value={valuesInContext}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/publier" element={<Publier />} />
+          <Route path="/favoris" element={<Favoris />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/profil" element={<Profil />} />
+        </Routes>
+      </Router>
+    </MyContext.Provider>
   );
 }
 
