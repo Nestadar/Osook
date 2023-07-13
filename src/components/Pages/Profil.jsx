8e406606fabc609622c9e61cd5783397fa94a101
Johanna Hilaire <johannahilairepro@gmail.com>
2023-07-13 03:54:51 +0200

    structure Profil

--- src/components/Pages/Profil.jsx
+++ src/components/Pages/Profil.jsx
@@ -1,9 +1,23 @@
-import "./Profil.scss";
+import React from "react";
 
 const Profil = () => {
   return (
-    <div>
-      <p>Profil</p>
+    <div className="global-Profile-container">
+      <img
+        className="profile-pic"
+        src="./src/assets/images/iconsNav/profilePic.png"
+        alt="Profile"
+      />
+      <div className="profile-Container">
+        <h1 className="title-Profile">Mon Profil</h1>
+        Login
+        <input type="text" placeholder="Nom" />
+        <input type="text" placeholder="Prénom" />
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Adresse" />
+        <input type="number" />
+        <input type="password" />
+      </div>
     </div>
   );
 };
